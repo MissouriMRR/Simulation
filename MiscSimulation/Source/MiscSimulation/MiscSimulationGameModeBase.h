a8c4ed1cd0da1a11f1788e00b96f1547d6b2d825
Gibson, Andrew (S&T-Student) <amgzbf@umsystem.edu>
2021-10-05 18:40:55 -0500

    Initialized world

---
+++ MiscSimulation/Source/MiscSimulation/MiscSimulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MiscSimulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MISCSIMULATION_API AMiscSimulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
